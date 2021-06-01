b676e5e8f79ddd43dbdcaf0059c49076071a9829
victo <victo@192.168.1.159>
2021-06-01 21:06:49 +0200

    Chess in progress

--- prAjedrez2/src/game/ML.java
+++ prAjedrez2/src/game/ML.java
@@ -1,4 +1,4 @@
-package Game;
+package game;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -6,27 +6,15 @@ import java.awt.event.MouseMotionListener;
 
 public class ML extends MouseAdapter implements MouseMotionListener {
     private boolean isPressed = false;
-    private int x = 0, y = 0;
     
     @Override
     public void mousePressed(MouseEvent e) {
         isPressed = true;
-        this.x = e.getX();
-        this.y = e.getY();
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         isPressed = false;
-        this.x = e.getX();
-        this.y = e.getY();
-    }
-
-    public int getX() { 
-    	return this.x; 
-    }
-    public int getY() { 
-    	return this.y; 
     }
 
     public boolean isPressed() {

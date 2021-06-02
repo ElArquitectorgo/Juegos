0820242e427e9de1fef175ddefc017c039506737
victo <victo@192.168.1.159>
2021-06-02 10:34:13 +0200

    Chess

--- prAjedrez2/src/pieces/Piece.java
+++ prAjedrez2/src/pieces/Piece.java
@@ -1,14 +1,17 @@
 package pieces;
 
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.util.List;
 
 import game.Window;
 
 public abstract class Piece {
 	protected BufferedImage img;
-	protected String color;
+	protected enum Color {WHITE, BLACK};
 
+	abstract public List<Point> validMoves(int xi, int yi, int xf, int yf);
 	abstract public boolean isValid(int xi, int yi, int xf, int yf);
 
 	public void draw(Graphics2D g2, int x, int y) {
